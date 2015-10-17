c2ffb3960515a60baca7a469e5bfc5e1ae1dfeaf
Prithviraj billa <prithviraj.m.billa@gmail.com>
2015-10-18 02:06:39 +0530

    started django app

---
+++ django_app/inventory/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+	name = models.CharField(max_length=128,db_index¶=True)
+	category = models.CharField(max_length=128,db_index¶=True)
+
+	def __str__(self):
+		self.name
+
+class Sku(models.Model):
+	product_id = models.ForeignKey(Product)
+	quantity = models.IntegerField()
+	type_quantity = models.CharField(max_length=10)
+
+
+class User(models.Model):
+	user_name = models.CharField(max_length=128)
+	last_updated = models.DateTimeField()
+
+class PurchaseHistory(models.Model):
+	user_id = models.ForeignKey(User)
+	sku_id = models.ForeignKey(Sku)
+	purchase_datetime = models.DateTimeField()
+	pruchase_time = models.TimeField()
+
+class Predictions(models.Model):
+	user_id = models.ForeignKey(User)
+	category = models.CharField(max_length=128)
+	predicted_datetime = models.DateTimeField()
+	predicted_time = models.TimeField()
