37440d37cf774d330dbae141f3d05818a3a901cb
Sushant Hiray <sushant@lumiata.com>
2015-10-18 18:07:02 +0530

    Adding PushNotification

---
+++ TheGrocerApp/app/src/main/java/com/grocerapp/thegrocerapp/QuickstartPreferences.java
@@ -0,0 +1,11 @@
+package com.grocerapp.thegrocerapp;
+
+/**
+ * Created by sushant on 10/18/15.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
