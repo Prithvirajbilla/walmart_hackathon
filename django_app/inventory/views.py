fef89ebf78b5d5df98857267fead4ac4edc64c51
Prithviraj billa <prithviraj.m.billa@gmail.com>
2015-10-18 12:07:12 +0530

    started off with templates

--- django_app/inventory/views.py
+++ django_app/inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse,HttpResponseRedirect,Http404,HttpResponseNotFound
+import json
+import datetime
+import urllib 
 
-# Create your views here.
+
+def home(request):
+	template="home.html"
+	return render(request,template)
